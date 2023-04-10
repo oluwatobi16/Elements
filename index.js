3fecbd67522c072b0ff95dfa418f8600e421c73a
oluwatobi16 <43851543+oluwatobi16@users.noreply.github.com>
2023-04-10 18:52:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,27 @@ window.addEventListener('load', function(event){
     fetch('http://127.0.0.1:8080/')
      .then(response => response.text())
      .then(body =>
-        document.getElementById('things').innerHTML=body)
+        document.getElementById('Chemicals').innerHTML=body)
         .catch((error) => alert(error))
   });
   
-  function renderThings (Fruits){
-    //alert(things);
-    let container = document.getElementById('things');
-    container.innerHTML= ''
-    for(let fruit of Fruits){
-       let item = document.createElement('li')
-       item.innerHTML = fruit;
-       container.appendChild(item);
-    }
-    }  
+ 
+    function renderElements (Chemicals){
+      //alert(things);
+      let container = document.getElementById('Elements');
+      for(let chemical of Chemicals){
+         let item = document.createElement('li')
+         item.innerHTML = chemical;
+         container.appendChild(item);
+      }
+      }
+      function validateForm() {
+         var x = document.forms["InputForm"]["newthing"].value;
+         if (x == "") {
+           alert("Name must be filled out");
+           return false;
+         }
+         else{
+           alert(x);
+         }
+       } 
