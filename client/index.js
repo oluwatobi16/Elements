23a3b6cc8a216977b046f8525bbad72c585c7dab
oluwatobi16 <43851543+oluwatobi16@users.noreply.github.com>
2023-04-22 13:01:00 +0100

    Update index.js

--- client/index.js
+++ client/index.js
@@ -1,49 +1,54 @@
-import Chemicals from "../server"
-let chemical=[]
-let data=JSON.parse(Chemicals);
-chemical.push(data)
 
-window.addEventListener('load', function(event){
-    fetch('http://127.0.0.1:8080/')
-     .then(response => response.text())
-     .then(body =>
-        document.getElementById('things').innerHTML=body)
-        .catch((error) => alert(error))
-  });
+//Initial Get request
+window.addEventListener('load', async function(event) {
+    try{
+   let response = await fetch('http://127.0.0.1:8090/list');
+    let body = await response.json();
+    renderThings(body);
+    } catch(e) {
+       alert(e);
+    }
+ })
+//function to dynamically load all Elements to page once fetched
+ function renderThings(Chemicals){
+   let container = document.getElementById('things')
+   container.innerHTML= '';
+   for (let i = 0; i < Chemicals.length; i++){
+      let obj = Chemicals[i]
+      var x = document.createElement('tr')
+      x.setAttribute('id','' + Chemicals.indexOf(i))
+      container.appendChild(x);
+
+      var element_name = document.createElement('td')
+      element_name.innerHTML = obj.name
+      x.appendChild(element_name)
+
+      var element_group = document.createElement('td')
+      element_group.innerHTML = obj.group
+      x.appendChild(element_group)
+      
+      var element_symbol = document.createElement('td')
+      element_symbol.innerHTML = obj.symbol
+      x.appendChild(element_symbol)
+
+      var element_price = document.createElement('td')
+      element_price.innerHTML = obj.price
+      x.appendChild(element_price)
+      
+      
+      var amount = document.createElement('td')
+      amount.innerHTML=0
+      x.appendChild(amount)
+      var increase= document.createElement('button')
+      increase.innerHTML = '+'
+      increase.setAttribute('onclick','Increment')
+      increase.setAttribute('id', i)
+      var decrease=document.createElement('button')
+      decrease.innerHTML = '-'
+      decrease.setAttribute('onclick','Decrement')
+      decrease.setAttribute('id', '-' + i)
+      amount.appendChild(increase)
+      amount.appendChild(decrease)
+   }
   
- 
-    function renderElements (chemical){
-      //alert(things);
-      let container = document.getElementById('things');
-      for(let element of chemical){
-         let item = document.createElement('li')
-         item.innerHTML = element;
-         container.appendChild(item);
-      }
-      }
-  submit.addEventListener('click',async function(event){
-          event.preventDefault();
-          let newthing = document.getElementById('newthing').value;
-          let parameters = {'newthing': newthing};
-          let response = await fetch('http://127.0.0.1:8080/element/add',{
-          method: 'POST',
-          headers: {
-             'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(parameters)
-       });
-       let body = await response.json();
-       renderThings(body);
-       })
-
-       function validateForm() {
-        var x = document.forms["InputForm"]["newthing"].value;
-        if (x == "") {
-          alert("Name must be filled out");
-          return false;
-        }
-        else{
-          alert(x);
-        }
-      } 
-      let submit = document.getElementById('submit_thing');
+ }
